eb94d6c661e3f44f55c8571d3a5081037e54d5fe
Kyle McGowan <kyle@windfallstudio.com>
2021-05-18 11:11:24 -0600

    Removes classes from other screen sizes if current size isn't specified.

--- maui.js
+++ maui.js
@@ -1,1 +1,103 @@
-const small=window.matchMedia("(max-width: 767px)"),medium=window.matchMedia("(min-width:767px) and (max-width: 1023px)"),large=window.matchMedia("(min-width: 1023px)");function swapClasses(t){small.matches&&document.querySelectorAll("[small]").forEach(t=>{var e=t.getAttribute("all"),a=t.getAttribute("small");t.removeAttribute("class"),null!=e&&(t.classList+=" "+e),t.classList+=" "+a});medium.matches&&document.querySelectorAll("[medium]").forEach(t=>{var e=t.getAttribute("all"),a=t.getAttribute("medium");t.removeAttribute("class"),null!=e&&(t.classList+=" "+e),t.classList+=" "+a});large.matches&&document.querySelectorAll("[large]").forEach(t=>{var e=t.getAttribute("all"),a=t.getAttribute("large");t.removeAttribute("class"),null!=e&&(t.classList+=" "+e),t.classList+=" "+a})}function onResizeFunction(t){swapClasses()}function onLoadFunction(t){swapClasses()}window.addEventListener("resize",onResizeFunction),window.addEventListener("load",onLoadFunction);
+const small   = window.matchMedia('(max-width: 767px)')
+const medium  = window.matchMedia('(min-width:767px) and (max-width: 1023px)')
+const large   = window.matchMedia('(min-width: 1023px)')
+
+function clearClasses (e){
+  var sms   = document.querySelectorAll('[small]');
+  var meds  = document.querySelectorAll('[medium]');
+  var lgs   = document.querySelectorAll('[large]');
+
+  sms.forEach(sm => {
+    var allClasses     = sm.getAttribute('all');
+    sm.removeAttribute("class");
+    if (allClasses != null){
+        sm.classList += ' ' + allClasses;
+    }
+  });
+
+  meds.forEach(md => {
+    var allClasses     = md.getAttribute('all');
+    md.removeAttribute("class");
+    if (allClasses != null){
+        md.classList += ' ' + allClasses;
+    }
+  });
+
+  lgs.forEach(lg => {
+    var allClasses     = lg.getAttribute('all');
+    lg.removeAttribute("class");
+    if (allClasses != null){
+        lg.classList += ' ' + allClasses;
+    }
+  });
+}
+
+function swapClasses (e){
+
+  clearClasses();
+
+  if (small.matches) {
+    var refs = document.querySelectorAll('[small]');
+
+    refs.forEach(ref => {
+      var allClasses     = ref.getAttribute('all');
+      var smallClasses  = ref.getAttribute('small');
+
+      ref.removeAttribute("class");
+
+      if (allClasses != null){
+          ref.classList += ' ' + allClasses;
+      }
+
+      ref.classList += ' ' + smallClasses;
+  	});
+  }
+
+  if (medium.matches) {
+    var refs = document.querySelectorAll('[medium]');
+
+    refs.forEach(ref => {
+      var allClasses     = ref.getAttribute('all');
+      var mediumClasses  = ref.getAttribute('medium');
+
+      ref.removeAttribute("class");
+
+      if (allClasses != null){
+          ref.classList += ' ' + allClasses;
+      }
+
+      ref.classList += ' ' + mediumClasses;
+  	});
+  }
+
+  if (large.matches) {
+    var refs = document.querySelectorAll('[large]');
+
+    refs.forEach(ref => {
+      var allClasses     = ref.getAttribute('all');
+      var largeClasses  = ref.getAttribute('large');
+
+      ref.removeAttribute("class");
+
+      if (allClasses != null){
+          ref.classList += ' ' + allClasses;
+      }
+
+      ref.classList += ' ' + largeClasses;
+  	});
+
+  }
+
+}
+
+// onLoad or onResize
+function onResizeFunction (e){
+  swapClasses();
+}
+
+function onLoadFunction(e){
+  swapClasses();
+}
+
+window.addEventListener("resize", onResizeFunction);
+window.addEventListener("load", onLoadFunction);
